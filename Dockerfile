1271e91e43f71faae96934e65cff00a47bb1e96f
Brandon Briones Rojas <brandon20kaido@gmail.com>
2026-09-01 00:34:04 -0500

    servicio gestor de usuarios

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM gradle:8.10.2-jdk21 AS builder
+
+WORKDIR /app
+
+COPY . .
+
+RUN gradle clean bootJar --no-daemon
+
+FROM eclipse-temurin:21-jre
+
+WORKDIR /app
+
+COPY --from=builder /app/build/libs/*.jar app.jar
+
+EXPOSE 8081
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/test/java/com/pragma/reto_aws/RetoAwsApplicationTests.java
+++ src/test/java/com/pragma/reto_aws/RetoAwsApplicationTests.java
@@ -3,11 +3,14 @@ package com.pragma.reto_aws;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+		properties = {
+				"spring.autoconfigure.exclude=org.springframework.boot.r2dbc.autoconfigure.R2dbcAutoConfiguration"
+		}
+)
 class RetoAwsApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
