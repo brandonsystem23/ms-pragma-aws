a5f85155bddf7b8b2c0154ca9a26a73e048082a1
Brandon Briones Rojas <brandon20kaido@gmail.com>
2026-09-02 09:46:01 -0500

    servicio gestor de usuarios

--- src/main/java/com/pragma/reto_aws/infrastructure/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/pragma/reto_aws/infrastructure/exceptionhandler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.pragma.reto_aws.infrastructure.exceptionhandler;
 
 import com.pragma.reto_aws.dominio.exception.DomainErrorCode;
 import com.pragma.reto_aws.dominio.exception.DomainException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,24 +14,29 @@ import java.time.ZoneId;
 import java.util.List;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DomainException.class)
     public ResponseEntity<ErrorResponse> handleDomainException(DomainException ex, ServerWebExchange exchange) {
         HttpStatus status = mapStatus(ex.getCode());
+        log.error("DomainException status:{} message:{}", status, ex.getMessage());
         return buildResponse(status, ex.getMessage(), exchange, List.of());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalArgumentException ex, ServerWebExchange exchange) {
+        log.error("IllegalArgumentException status:{} message:{}", HttpStatus.BAD_REQUEST, ex.getMessage());
         return buildResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), exchange, List.of());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGenericException(Exception ex, ServerWebExchange exchange) {
+        String message = "Ocurrió un error interno en el servidor";
+        log.error("Exception status:{} message:{}", HttpStatus.INTERNAL_SERVER_ERROR, message);
         return buildResponse(
                 HttpStatus.INTERNAL_SERVER_ERROR,
-                "Ocurrió un error interno en el servidor",
+                message,
                 exchange,
                 List.of()
         );
